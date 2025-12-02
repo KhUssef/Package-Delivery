a25235c7116f70db09582977ff343c7f0fb79762
KhUssef <khalifayoussef628@gmail.com>
2025-12-02 20:15:27 +0100

    fixed1

--- src/main/java/ai/proj/DeliverySearch.java
+++ src/main/java/ai/proj/DeliverySearch.java
@@ -208,13 +208,6 @@ public class DeliverySearch {
         System.out.println(this.initialState);
         System.out.println(this.trafficString);
         this.searchAlgorithm.extract(this.initialState, this.trafficString);
-<<<<<<< HEAD
-        this.searchAlgorithm = new IterativeDeepening();
-        this.searchAlgorithm.extract(initialState, trafficString);
-        System.out.println(this.searchAlgorithm.search(new String(this.destinations[0][0] + "," + this.destinations[0][1])));
-=======
-        System.out.println("DFS: " + this.searchAlgorithm.search(this.initialState + ";" + 0));
->>>>>>> 694ff923ef734aebf9d98e0e27e8cb53dff64791
     }
 
     String path(String initialState, String traffic, String goalState) {
