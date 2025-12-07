4fe3bf4e10eabbe2ad45bcb1c589e399aaad6ca3
S3ID420 <oussamawaid@gmail.com>
2025-12-07 17:07:17 +0100

    cors

--- src/main/java/ai/proj/PlannerController.java
+++ src/main/java/ai/proj/PlannerController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api")
 public class PlannerController {
     private final DeliveryPlanner planner = new DeliveryPlanner();
